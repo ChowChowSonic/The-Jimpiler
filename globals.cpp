4697cf7a1f9e98ead1e3d1300e32e5611a323e03
ChowChowSonic <joeyant1367@gmail.com>
2025-03-06 22:35:38 -0500

    moved last few lines of function call invocations to its own function for reusability

--- globals.cpp
+++ globals.cpp
@@ -11,9 +11,10 @@
 #include "tokenizer.cpp"
 #include "AliasManager.h"
 #include "TemplateGenerator.cpp"
-namespace jimpilier{
-	class AliasManager; 
-    /*
+namespace jimpilier
+{
+	class AliasManager;
+	/*
 	 * General Naming rule:
 	 * "___Stmt" = anything that does more than it may appear on the back end
 	 * "___Expr" = Anything that does what it can in a relatively straightforward way
@@ -23,39 +24,60 @@ namespace jimpilier{
 	std::unique_ptr<llvm::Module> GlobalVarsAndFunctions;
 	std::unique_ptr<llvm::DataLayout> DataLayout;
 	AliasManager AliasMgr;
-	TemplateGenerator TemplateMgr; 
+	TemplateGenerator TemplateMgr;
 	std::vector<std::string> importedFiles;
 	llvm::Function *currentFunction;
-	llvm::BasicBlock *currentUnwindBlock = NULL; 
-    /**
+	llvm::BasicBlock *currentUnwindBlock = NULL;
+	/**
 	 * @brief function where any code put into a static block is placed. Eventually all code in this will be drained into the beginning of main() if it exists
-	 * 
+	 *
 	 */
 	llvm::Function *STATIC = nullptr;
 	/**
 	 * @brief In the event that a Break statement is called (I.E. in a for loop or switch statements),
-	 * the compilier will automatically jump to the top llvm::BasicBlock in this stack. 
+	 * the compilier will automatically jump to the top llvm::BasicBlock in this stack.
 	 * The first block in the pair is for `continue` keywords, the second is for `break` keywords.
 	 *
 	 */
 	std::stack<std::pair<llvm::BasicBlock *, llvm::BasicBlock *>> escapeBlock;
 	std::map<llvm::Type *, std::map<std::string, std::map<llvm::Type *, llvm::Function *>>> operators;
-	std::map<llvm::Type*, llvm::Value*> classInfoVals; 
-	std::string currentFile; 
+	std::map<llvm::Type *, llvm::Value *> classInfoVals;
+	std::string currentFile;
 	/* Strictly for testing purposes, not meant for releases*/
 	const bool DEBUGGING = false;
 
-		// <-- BEGINNING OF UTILITY FUNCTIONS -->
+	// <-- BEGINNING OF UTILITY FUNCTIONS -->
 
-	void logError(const std::string& s, Token t)
+	void logError(const std::string &s, Token t)
 	{
 		std::cout << s << ' ' << t.toString() << std::endl;
 		assert(false);
 	}
-	void logError(const std::string& s)
+	void logError(const std::string &s)
 	{
-		std::cout << s << ' '<<std::endl; 
-		assert(false); 
+		std::cout << s << ' ' << std::endl;
+		assert(false);
+	}
+
+	llvm::Value *makeCallWithReferences(std::vector<llvm::Value *> &ptrsToArgs, FunctionHeader &CalleeF)
+	{
+		for (unsigned i = 0; i < ptrsToArgs.size(); ++i)
+		{
+			if (!CalleeF.args[i].isRef && ptrsToArgs[i]->getType()->getPointerTo() == CalleeF.args[i].ty)
+				ptrsToArgs[i] = builder->CreateLoad(ptrsToArgs[i]->getType()->getNonOpaquePointerElementType(), ptrsToArgs[i], "dereftmp");
+			if (!ptrsToArgs[i])
+			{
+				assert(false && "Error saving function args");
+			}
+		}
+		if (!CalleeF.canThrow())
+			return CalleeF.func->getReturnType() == llvm::Type::getVoidTy(*ctxt) ? builder->CreateCall(CalleeF.func, ptrsToArgs) : builder->CreateCall(CalleeF.func, ptrsToArgs, "calltmp");
+		assert(currentUnwindBlock != NULL && "Attempted to call a function that throws errors with no way to catch the error!");
+		llvm::BasicBlock *normalUnwindBlock = llvm::BasicBlock::Create(*ctxt, "NormalExecBlock", currentFunction);
+		llvm::Value *retval = CalleeF.func->getReturnType() == llvm::Type::getVoidTy(*ctxt) ? builder->CreateInvoke(CalleeF.func, normalUnwindBlock, currentUnwindBlock, ptrsToArgs) : builder->CreateInvoke(CalleeF.func, normalUnwindBlock, currentUnwindBlock, ptrsToArgs, "calltmp");
+		builder->SetInsertPoint(normalUnwindBlock);
+		return retval;
 	}
 }
+
 #endif
