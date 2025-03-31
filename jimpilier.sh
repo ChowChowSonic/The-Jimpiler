6b9a9f5f9a5cc46476fc482332df612e68ed246a
ChowChowSonic <joeyant1367@gmail.com>
2025-03-31 19:56:35 -0400

    updated shell setup file & YAML, gitignore

--- jimpilier.sh
+++ jimpilier.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 g++ -g -O3 -c `llvm-config --cxxflags --ldflags --system-libs --libs core` $1 -o unlinked_exe && 
-g++ unlinked_exe $(llvm-config --ldflags --libs) -lpthread -o jmb
-rm -rf unlinked_exe
+g++ unlinked_exe $(llvm-config --ldflags --libs) -lpthread -o jmb &&
+rm -rf unlinked_exe &&
+g++ -std=c++14 -o runTests -Wl,--copy-dt-needed-entries -lgtest_main -lgtest  `ls tests/*`
