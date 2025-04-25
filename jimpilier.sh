c5adc4eb333900ecd67eecd6e482a84dcdcb05a0
ChowChowSonic <joeyant1367@gmail.com>
2025-04-25 16:04:44 -0400

    enabled execption catching for build purposes

--- jimpilier.sh
+++ jimpilier.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ -g -O3 -c `llvm-config --cxxflags --ldflags --system-libs --libs core` $1 -o unlinked_exe && 
-g++ unlinked_exe $(llvm-config --ldflags --libs) -lpthread -o jmb &&
+g++ -g -O3 -c -fexceptions -lfmt `llvm-config --cxxflags --ldflags --system-libs --libs core | sed 's/-fno-exceptions//g'` $1 -o unlinked_exe && 
+g++ unlinked_exe $(llvm-config --ldflags --libs) -lfmt -lpthread -o jmb &&
 rm -rf unlinked_exe &&
 g++ -std=c++14 -o runTests -Wl,--copy-dt-needed-entries -lgtest_main -lgtest  `ls tests/*`
